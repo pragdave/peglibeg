f56f31e0e3cb4740326859553dde976f751cb46c
kenziesl <31229103+kenziesl@users.noreply.github.com>
2019-11-25 22:20:28 -0600

    Update visitor.h

--- visitor.h
+++ visitor.h
@@ -1,11 +1,17 @@
 #pragma once
 
+
+
+
 class AstNode;
 
 using namespace std;
 
 class Visitor {
 public:
+
+	Visitor() {};
+
 	virtual int evaluate_integer(AstNode *node, int value) 
 		{ return 0;};
 	virtual int evaluate_binop(AstNode *node, AstNode *left, string op, AstNode *right) 
@@ -16,6 +22,10 @@ public:
 		{ return 0; };     
 	virtual int evaluate_if_expr(AstNode *node, AstNode *left, AstNode *right, AstNode *el) 
 		{ return 0; };
-	virtual int evaluate_block(AstNode *node, vector<AstNode*> nodes, int numNodes)
+	virtual int evaluate_block(AstNode *node, vector<AstNode*> nodes)
+		{ return 0; };
+	virtual int evaluate_funcall(AstNode *node, vector<string> names, vector<AstNode*> nodes, AstNode* block)
+		{ return 0; };
+	virtual AstNode* find_func(string name)
 		{ return 0; };
 };
