fec6277ab7a91a7b38a964cfb6c1123b0f9296ba
kenziesl <31229103+kenziesl@users.noreply.github.com>
2019-11-25 22:18:26 -0600

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -24,10 +24,13 @@ class Interpreter : public Visitor {
 	Binding bindings;
 
 public:
+	Interpreter* getInterpreter(Binding& b);
 	int evaluate_integer(AstNode *node, int value);
 	int evaluate_binop(AstNode *node, AstNode *left, string op, AstNode *right);
 	int evaluate_varref(AstNode *node, string name);
 	int evaluate_assignment(AstNode* node, AstNode* left, string op, AstNode* right);
 	int evaluate_if_expr(AstNode *node, AstNode *left, AstNode *right, AstNode *el);
-	int evaluate_block(AstNode *node, vector<AstNode*> nodes, int numNodes);
+	int evaluate_block(AstNode *node, vector<AstNode*> nodes);
+	int evaluate_funcall(AstNode *node, vector<string> names, vector<AstNode*> nodes, AstNode* block);
+	AstNode* find_func(string name);
 };
