d0ebf65739b1035b5628166bbff5153704cbfcb8
kenziesl <31229103+kenziesl@users.noreply.github.com>
2019-11-25 22:16:36 -0600

    Update binding.cc

--- binding.cc
+++ binding.cc
@@ -2,19 +2,23 @@
 
 using namespace std;
 
-Binding::Binding () {
-	bindings.insert( pair<string, int>("x", 5) );
-}
+Binding::Binding () {}
 
-void Binding::set_variable(string name, int val) {
-	map<string, int>::iterator it = bindings.find(name);
+void Binding::set_variable(string& name, AstNode*& val) {
+	map<string, AstNode*>::iterator it = bindings.find(name);
 	if (it != bindings.end()) {
 		it->second = val;
 	} else {
-		bindings.insert( pair<string, int>(name, val) );
+		bindings.insert( pair<string, AstNode*>(name, val) );
 	}
 }
 
-int Binding::get_variable_value(string name) {
+AstNode* Binding::get_variable_value(string& name) {
 	return bindings.find(name)->second;
 }
+
+Binding& Binding::operator = (Binding& b) {
+	this->bindings = b.bindings;
+	return *this;
+}
+
