b0166b41d4b3af7959a449816364205af3daf379
kenziesl <31229103+kenziesl@users.noreply.github.com>
2019-11-25 22:17:14 -0600

    Update binding.h

--- binding.h
+++ binding.h
@@ -2,12 +2,15 @@
 #include <map>
 #include <iostream>
 
+#include "ast_node.h"
+
 using namespace std;
 
 class Binding {
-	map<string, int> bindings;
+	map<string, AstNode*> bindings;
 public:
 	Binding();
-	void set_variable(string, int val);
-	int get_variable_value(string name);
+	void set_variable(string&, AstNode*& val);
+	AstNode* get_variable_value(string& name);
+	Binding& operator=(Binding& b);
 };
